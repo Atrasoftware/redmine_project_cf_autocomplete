2a2b60b05baa927e1d1897d70898e0070558e27f
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-14 17:07:45 +0100

    integrate select 2

--- lib/patches/projects_controller_patch_rpca.rb
+++ lib/patches/projects_controller_patch_rpca.rb
@@ -21,16 +21,18 @@ module  Patches
       @hash = Hash.new
       cf_options = setting[:choosed_cf].split('|')
       CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| setting[:choosed_cf].include?(col.id.to_s) }.each do |cf|
-        projects = Project.visible
-        available_tag = Array.new
-        projects.each do |project|
-          project.visible_custom_field_values.select{|coll| coll.custom_field.name == cf.name }.each do |custom_value|
-            unless custom_value.value.blank?
-              available_tag<<custom_value.value
+       if cf.field_format != 'list'
+          projects = Project.visible
+          available_tag = Array.new
+          projects.each do |project|
+            project.visible_custom_field_values.select{|coll| coll.custom_field.name == cf.name }.each do |custom_value|
+              unless custom_value.value.blank?
+                available_tag<< custom_value.value.html_safe
+              end
             end
           end
-        end
-        @hash[cf.id.to_s] = available_tag
+          @hash[cf.id.to_s] = available_tag
+         end
       end
     end
 
