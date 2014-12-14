8560bfbb59cae12397ae60ef76ce90ddcdd6b003
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-14 21:18:48 +0100

    fix &quot; bug: convertion between ruby too jquery

--- lib/patches/projects_controller_patch_rpca.rb
+++ lib/patches/projects_controller_patch_rpca.rb
@@ -21,19 +21,25 @@ module  Patches
       @hash = Hash.new
       cf_options = setting[:choosed_cf].split('|')
       CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| setting[:choosed_cf].include?(col.id.to_s) }.each do |cf|
-        if cf.field_format != 'list'
+
           projects = Project.visible
           available_tag = Array.new
           projects.each do |project|
             project.visible_custom_field_values.select{|coll| coll.custom_field.name == cf.name }.each do |custom_value|
               unless custom_value.value.blank?
-                available_tag<< custom_value.value.html_safe
+                if cf.field_format != 'list'
+                  available_tag<< custom_value.value.html_safe
+                else
+                  available_tag<< custom_value.value
+                end
               end
             end
-          end
+
           @hash[cf.id.to_s] = available_tag
         end
       end
+
+
     end
 
   end
